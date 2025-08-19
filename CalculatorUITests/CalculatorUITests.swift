e228192ee6f47d7c7a8ad10464f6e66e855acd0f
ZaminOrucov <zamin.h.orujov@gmail.com>
2025-08-19 12:51:37 +0400

    Initial Commit

---
+++ CalculatorUITests/CalculatorUITests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorUITests.swift
+//  CalculatorUITests
+//
+//  Created by Zamin Orucov on 8/19/25.
+//
+
+import Testing
+@testable import CalculatorUI
+
+struct CalculatorUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
